1b27b971cf3714e0b6faa78bcd41143eb6d48902
domine29 <eric.dominguez29@gmail.com>
2022-12-30 17:51:50 -0600

    added fixture for all card data in the tarot_app folder

--- backend/journaling_app/models.py
+++ backend/journaling_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from tarot_app.models import SiteUser
 
-# Create your models here.
+class Journal(models.Model):
+    user = models.OneToOneField('tarot_app.SiteUser', on_delete=models.CASCADE)
+    # table_of_contents = 
+
+class DreamEntry(models.Model):
+    journal = models.ForeignKey(Journal, on_delete=models.CASCADE)
+    interpretation = models.TextField()
+    ritual = models.TextField()
+
+class DreamImage(models.Model):
+    dream_entry = models.ForeignKey(DreamEntry, on_delete=models.CASCADE)
+    description = models.TextField()
+    associations = models.TextField()
+    inner_dynamics = models.TextField()
